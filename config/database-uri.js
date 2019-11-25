88595d0f55e90323795b610d7c587e73b540bded
YuriBeans <ysoaresmkt@gmail.com>
2019-11-25 04:33:05 -0300

    Ajuste configuração do banco de dados e variáveis de ambiente

--- config/database-uri.js
+++ config/database-uri.js
@@ -2,8 +2,10 @@ require('dotenv').config({
     path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 });
       
+const DB_CLOUD = process.env.DB_CLOUD;
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
 const DB_NAME = process.env.DB_NAME;
-      
+
+let uri =  `${DB_CLOUD}://${DB_NAME}:${DB_PASSWORD}>${DB_HOST}`;
 module.exports = uri;
--- config/database.js
+++ config/database.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose');
 const uri = require('./database-uri');
+
+global.db = (global.db ? global.db : mongoose)
+
+db.connect(uri, {
+  useNewUrlParser: true
+}).then(result => {
+  console.log('MongoDB Conectado', uri)
+}).catch(error => {
+  console.log(error)
+})
+
+module.exports = uri
